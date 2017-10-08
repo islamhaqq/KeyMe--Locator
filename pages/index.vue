7adc945fe9b4b9725b298cdf33582c2fa028b9bd
Syed Aman <sna274@nyu.edu>
2017-10-08 11:16:29 -0500

    create the locations card that display a list of kiosk locations in a card. The data is retrieved from the vuex store

--- pages/index.vue
+++ pages/index.vue
@@ -4,20 +4,24 @@
       <h1 class="headline text-xs-center">Find the Closest Kiosk</h1>
       <v-card>
         <v-card-title class="title">Locations</v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>For more information on Vuetify, check out the <a href="https://vuetifyjs.com" target="_blank">documentation</a>.</p>
-          <p>If you have questions, please join the official <a href="https://chat.vuetifyjs.com/" target="_blank" title="chat">discord</a>.</p>
-          <p>Find a bug? Report it on the github <a href="https://github.com/vuetifyjs/vuetify/issues" target="_blank" title="contribute">issue board</a>.</p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a href="https://nuxtjs.org/" target="_blank">Nuxt Documentation</a>
-          <br>
-          <a href="https://github.com/nuxt/nuxt.js" target="_blank">Nuxt GitHub</a>
-        </v-card-text>
+        <!-- <v-card-text> -->
+          <v-list>
+            <v-list-tile v-for="location of locations" :key="location.id" three-line class="mb-3">
+              <v-list-tile-action>
+                <v-icon color="primary">location_on</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>{{ location.retailer }}</v-list-tile-title>
+                <v-list-tile-sub-title>
+                  {{ `${location.city}, ${location.state}` }}
+                </v-list-tile-sub-title>
+                <v-list-tile-sub-title>
+                  {{ location.address }}
+                </v-list-tile-sub-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+        <!-- </v-card-text> -->
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn primary flat>Request Local Kiosk</v-btn>
@@ -26,3 +30,13 @@
     </v-flex>
   </v-layout>
 </template>
+
+<script>
+  export default {
+    computed: {
+      locations () {
+        return this.$store.state.locations
+      }
+    }
+  }
+</script>
--- store/index.js
+++ store/index.js
@@ -1,5 +1,55 @@
 export const state = () => ({
-  sidebar: false
+  locations: [
+    {
+      id: 'asdoiasdi2132193OIODioasd234',
+      retailer: 'Rite Aid',
+      address: '85-10 Northern Boulevard, Jackson Heights, NY, 11372',
+      city: 'Jackson Heights',
+      state: 'NY'
+    },
+    {
+      id: 'zxpocxzopc2131242421',
+      retailer: '7 Eleven',
+      address: '85-10 Northern Boulevard, Jackson Heights, NY, 11372',
+      city: 'Jackson Heights',
+      state: 'NY'
+    },
+    {
+      id: 'zxpocxzopc2131242421',
+      retailer: '7 Eleven',
+      address: '85-10 Northern Boulevard, Jackson Heights, NY, 11372',
+      city: 'Jackson Heights',
+      state: 'NY'
+    },
+    {
+      id: 'zxpocxzopc2131242421',
+      retailer: '7 Eleven',
+      address: '85-10 Northern Boulevard, Jackson Heights, NY, 11372',
+      city: 'Jackson Heights',
+      state: 'NY'
+    },
+    {
+      id: 'zxpocxzopc2131242421',
+      retailer: '7 Eleven',
+      address: '85-10 Northern Boulevard, Jackson Heights, NY, 11372',
+      city: 'Jackson Heights',
+      state: 'NY'
+    },
+    {
+      id: 'zxpocxzopc2131242421',
+      retailer: '7 Eleven',
+      address: '85-10 Northern Boulevard, Jackson Heights, NY, 11372',
+      city: 'Jackson Heights',
+      state: 'NY'
+    },
+    {
+      id: 'zxpocxzopc2131242421',
+      retailer: '7 Eleven',
+      address: '85-10 Northern Boulevard, Jackson Heights, NY, 11372',
+      city: 'Jackson Heights',
+      state: 'NY'
+    }
+  ]
 })
 
 export const mutations = {
