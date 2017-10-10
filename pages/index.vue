f4096fde7bfa5e1020cb3a0bbf1902929a3cf6dd
Syed Aman <sna274@nyu.edu>
2017-10-10 04:22:07 -0500

    add/remove comments

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,8 @@
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6>
       <h1 class="headline text-xs-center">Find the Closest Kiosk</h1>
+
+      <!-- Google Maps -->
       <v-card
         img="/google-maps.jpg"
         flat
@@ -16,32 +18,30 @@
           <v-btn icon>
             <v-icon>my_location</v-icon>
           </v-btn>
-          <v-btn icon>
-            <v-icon>more_vert</v-icon>
-          </v-btn>
         </v-toolbar>
       </v-card>
 
+      <!-- Kiosk Locations -->
       <v-card>
         <v-card-title class="title">Locations</v-card-title>
-        <!-- <v-card-text> -->
-          <v-list>
-            <v-list-tile v-for="location of locations" :key="location.id" three-line class="mb-3">
-              <v-list-tile-action>
-                <v-icon color="primary">location_on</v-icon>
-              </v-list-tile-action>
-              <v-list-tile-content>
-                <v-list-tile-title>{{ location.retailer }}</v-list-tile-title>
-                <v-list-tile-sub-title>
-                  {{ `${location.city}, ${location.state}` }}
-                </v-list-tile-sub-title>
-                <v-list-tile-sub-title>
-                  {{ location.address }}
-                </v-list-tile-sub-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-list>
-        <!-- </v-card-text> -->
+
+        <v-list>
+          <v-list-tile v-for="location of locations" :key="location.id" three-line class="mb-3">
+            <v-list-tile-action>
+              <v-icon color="primary">location_on</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ location.retailer }}</v-list-tile-title>
+              <v-list-tile-sub-title>
+                {{ `${location.city}, ${location.state}` }}
+              </v-list-tile-sub-title>
+              <v-list-tile-sub-title>
+                {{ location.address }}
+              </v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn primary flat>Request Local Kiosk</v-btn>
