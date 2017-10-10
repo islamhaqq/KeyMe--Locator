549591ebe608ef2a2d9b9a9ab4d0f14fdaad1aeb
Syed Aman <sna274@nyu.edu>
2017-10-10 04:22:36 -0500

    import a-la-carte textfield and dialog to use for the kiosk locator

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -10,7 +10,8 @@ import {
   VIcon,
   VGrid,
   VToolbar,
-  VTextField
+  VTextField,
+  VDialog
 } from 'vuetify'
 
 Vue.use(Vuetify, {
@@ -24,6 +25,7 @@ Vue.use(Vuetify, {
     VIcon,
     VGrid,
     VToolbar,
-    VTextField
+    VTextField,
+    VDialog
   }
 })
