6f50854c8dcb0939809ad541b5e9a702433e7f0b
Syed Aman <sna274@nyu.edu>
2017-10-11 12:30:44 -0500

    configure gmaps api key and libraries

--- plugins/vue-google-maps.js
+++ plugins/vue-google-maps.js
@@ -1,8 +1,14 @@
 /**
- * Installation of Vue's Google Maps service.
+ * Configuration & installation of Vue's Google Maps service.
  */
 
 import Vue from 'vue'
 import * as VueGoogleMaps from '~/node_modules/vue2-google-maps/src/main'
 
-Vue.use(VueGoogleMaps)
+// install the package while specifying the API key and GMAP libraries to use
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyDiDSE1n9M809VxrXaVjEFzV47mdGMAbHo',
+    libraries: 'places'
+  }
+})
