9ab7f10e49dfbfde5cfcd96f5b319a47dfc1e405
Syed Aman <sna274@nyu.edu>
2017-10-12 18:32:56 -0500

    add in the logo to the nav toolbar

--- components/NavToolbar.vue
+++ components/NavToolbar.vue
@@ -2,11 +2,22 @@
 
 <template>
   <v-toolbar fixed app>
-    <v-toolbar-title>KeyMe Kiosk Locator</v-toolbar-title>
+    <img src="~assets/key-me-logo.png"/>
+    <v-toolbar-title class="ml-0">
+      <span class="black--text">Key</span>
+      <span class="primary--text">Me</span>
+    </v-toolbar-title>
     <v-spacer></v-spacer>
+    <span class="title">Kiosk Locator</span>
   </v-toolbar>
 </template>
 
 <script>
   export default {}
 </script>
+
+<style lang="stylus" scoped>
+  img
+    height 100%
+    transform scale(.8)
+</style>
