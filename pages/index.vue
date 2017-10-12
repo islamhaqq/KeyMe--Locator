4b613aef71f588b4fa8ae582cbeb7c619944563b
Syed Aman <sna274@nyu.edu>
2017-10-12 14:53:17 -0500

    revert mistake commit

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@
       <v-card lat class="mb-2">
         <v-toolbar dense light>
           <v-text-field v-model="search" type="text" placeholder="Search locations..." prepend-icon="search" hide-details single-line light />
-          <v-btn icon>
+          <v-btn @click.native="updateMapToLocation" icon>
             <v-icon>my_location</v-icon>
           </v-btn>
         </v-toolbar>
@@ -115,6 +115,17 @@
           lng: position.coords.longitude
         })
       })
+    },
+    methods: {
+      /**
+       * Updates the Google Map to user's current location if GPS allowed.
+       * @method updateMapToLocation
+       * @return {Void}
+       */
+      updateMapToLocation () {
+        console.log('update map to location')
+        // do stuff
+      }
     }
   }
 </script>
