39fdca4e6f9d0979853801d4a666e2f1a060193f
Syed Aman <sna274@nyu.edu>
2017-10-13 17:37:57 -0500

    add documentation for the snackbar component

--- components/snackbar.vue
+++ components/snackbar.vue
@@ -1,7 +1,9 @@
-<!-- The app snackbar. -->
+<!-- The snackbar that displays in the bottom of the app. Can be configured to
+close or open whenever, provided with a message. -->
 
 <template>
   <v-snackbar v-model="isSnackbarOpen" bottom>
+    <!-- TODO: default message? -->
     <span>{{ snackbarMessage }}</span>
     <v-btn @click.native="isSnackbarOpen = false" flat color="pink">
       Close
@@ -17,15 +19,30 @@
        * @type {Boolean}
        */
       isSnackbarOpen: {
+        /**
+         * Gets whether the snackbar is open or not.
+         * @method get
+         * @return {Boolean} - Whether the snackbar is open or not.
+         */
         get () {
           return this.$store.state.isSnackbarOpen
         },
+        /**
+         * Opens or closes the snackbar.
+         * @method set
+         * @param  {Boolean} value - Whether to open orclose the snackbar.
+         */
         set (value) {
           this.$store.commit('toggleSnackbar', {
             display: value
           })
         }
       },
+      /**
+       * The text displaying in the snackbar.
+       * @method snackbarMessage
+       * @return {String} - The snackbar message.
+       */
       snackbarMessage () {
         return this.$store.state.snackbarMessage
       }
