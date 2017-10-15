e8f164ed5fda0e9e44aed578a2a42d1a67078e94
Syed Aman <sna274@nyu.edu>
2017-10-15 13:58:01 -0400

    create a FAB that opens the location details

---
+++ components/open-location-details-fab.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-btn v-if="showOpenLocationDetailsFAB" @click.native="toggleLocationDetails" color="accent" light small absolute bottom left fab>
+    <v-icon>store</v-icon>
+  </v-btn>
+</template>
+
+<script>
+  export default {
+    computed: {
+      showOpenLocationDetailsFAB () {
+        return this.$store.getters.filteredLocations.length === 1
+      }
+    },
+    methods: {
+      toggleLocationDetails () {
+        this.$store.commit('toggleLocationDetails', {
+          visible: true,
+          location: this.$store.getters.filteredLocations[0]
+        })
+      }
+    }
+  }
+</script>
