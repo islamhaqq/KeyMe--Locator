4449c99d4bc61054aa07c4edba8280bf05a14210
Syed Aman <sna274@nyu.edu>
2017-10-16 05:37:38 -0400

    componentize and refactor the kiosk locations card

---
+++ components/kiosk-locations-card.vue
@@ -0,0 +1,71 @@
+<template>
+  <!-- Kiosk Locations -->
+  <v-card>
+    <v-card-title class="title">
+      {{ `Locations (${filteredLocations.length})` }}
+    </v-card-title>
+
+    <!-- Listed kiosk locations. -->
+    <v-list three-line>
+      <!-- Clicking a location will update the GMap. -->
+      <v-list-tile v-for="location of filteredLocations" :key="location.id" @click="focusOnLocation(location)" class="mb-3">
+        <!-- Icon. -->
+        <v-list-tile-action>
+          <v-icon color="primary">location_on</v-icon>
+        </v-list-tile-action>
+
+        <!-- Kiosk location details. -->
+        <v-list-tile-content>
+          <v-list-tile-title>{{ location.retailer }}</v-list-tile-title>
+          <v-list-tile-sub-title>
+            {{ `${location.city}, ${location.state}` }}
+          </v-list-tile-sub-title>
+          <v-list-tile-sub-title class="accent--text">
+            {{ location.address }}
+          </v-list-tile-sub-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <!-- A button that enables users to request a kiosk in their location
+      if they don't have one around them or would like more. -->
+      <RequestKioskDialog />
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+  import RequestKioskDialog from '@/components/request-kiosk-dialog'
+  import LocationDetails from '@/components/location-details'
+
+  export default {
+    components: {
+      RequestKioskDialog,
+      LocationDetails
+    },
+    computed: {
+      /**
+       * Tries to get the kiosk location that the user is trying to search for.
+       * @method filteredLocations
+       * @return {Array} - An array of kiosk locations.
+       */
+      filteredLocations () {
+        return this.$store.getters.filteredLocations
+      }
+    },
+    methods: {
+      /**
+       * Update the search query so that GMaps focuses on the specified
+       * location, which triggers the location details FAB to display.
+       * @method focusOnLocation
+       * @param  {Object} location - The store/kiosk location meta.
+       * @return {Void}
+       */
+      focusOnLocation (location) {
+        this.$store.commit('updateSearch', location.address)
+      }
+    }
+  }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -5,77 +5,28 @@
     <v-flex xs12 sm8 md6>
       <h1 class="headline text-xs-center">Find the Closest Kiosk</h1>
 
-      <!-- Google Maps -->
+      <!-- A Google Maps card displaying kiosk locations. -->
       <GoogleMapsCard />
 
       <!-- Kiosk Locations -->
-      <v-card>
-        <v-card-title class="title">
-          {{ `Locations (${filteredLocations.length})` }}
-        </v-card-title>
-
-        <!-- Listed locations. -->
-        <v-list three-line>
-          <v-list-tile v-for="location of filteredLocations" :key="location.id" @click="focusOnLocation(location)" class="mb-3">
-            <v-list-tile-action>
-              <v-icon color="primary">location_on</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>{{ location.retailer }}</v-list-tile-title>
-              <v-list-tile-sub-title>
-                {{ `${location.city}, ${location.state}` }}
-              </v-list-tile-sub-title>
-              <v-list-tile-sub-title class="accent--text">
-                {{ location.address }}
-              </v-list-tile-sub-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list>
-
-        <!-- A button that enables users to request a kiosk in their location
-        if they don't have one around them or would like more. -->
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <RequestKioskDialog />
-        </v-card-actions>
-      </v-card>
+      <KioskLocationsCard />
     </v-flex>
+
+    <!-- Bottom sheet that opens up displaying kiosk/store details. -->
     <LocationDetails />
   </v-layout>
 </template>
 
 <script>
-  import RequestKioskDialog from '@/components/request-kiosk-dialog'
   import LocationDetails from '@/components/location-details'
   import GoogleMapsCard from '@/components/google-maps-card'
+  import KioskLocationsCard from '@/components/kiosk-locations-card'
 
   export default {
     components: {
-      RequestKioskDialog,
       LocationDetails,
-      GoogleMapsCard
-    },
-    computed: {
-      /**
-       * Tries to get the kiosk location that the user is trying to search for.
-       * @method filteredLocations
-       * @return {Array} - An array of kiosk locations.
-       */
-      filteredLocations () {
-        return this.$store.getters.filteredLocations
-      }
-    },
-    methods: {
-      /**
-       * Update the search query so that GMaps focuses on the specified
-       * location, which triggers the location details FAB to display.
-       * @method focusOnLocation
-       * @param  {Object} location - The store/kiosk location meta.
-       * @return {Void}
-       */
-      focusOnLocation (location) {
-        this.$store.commit('updateSearch', location.address)
-      }
+      GoogleMapsCard,
+      KioskLocationsCard
     }
   }
 </script>
